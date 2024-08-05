030d88bc62e39c2abb6d9dd378830f42a729eafc
BatoAlvin <alvinbato112@gmail.com>
2024-08-05 09:02:55 +0300

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Gate;
+
+class PostController extends Controller
+{
+    public function __construct()
+{
+    $this->middleware('auth:sanctum')->except(['index', 'show']);
+}
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorePostRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+       $fields = $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required',
+        ]);
+        $post = $request->user()->posts()->create($fields);
+        return ['post' => $post, 'user' => $post->user];
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        return ['post' => $post, 'user' => $post->user];
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatePostRequest  $request
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        Gate::authorize('modify', $post);
+
+        $fields = $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required',
+        ]);
+       $post->update($fields);
+        return $post;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        Gate::authorize('modify', $post);
+      $post->delete();
+      return ['message' => 'Post was deleted'];
+    }
+}
